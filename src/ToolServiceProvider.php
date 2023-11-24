7f3e757853f493b8c8b24f15e15813bf7e39ebb5
curder <q.curder@gmail.com>
2023-11-24 14:41:59 +0800

    Fixed can not use config method to get meddlewire value

--- src/ToolServiceProvider.php
+++ src/ToolServiceProvider.php
@@ -8,6 +8,8 @@ use Illuminate\Support\Facades\Route;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
+use function config;
+
 class ToolServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
@@ -18,13 +20,6 @@ class ToolServiceProvider extends PackageServiceProvider
             ->hasTranslations()
             ->hasRoutes('api', 'inertia');
 
-        Nova::router(config('password-reset.middleware'), 'password-reset')
-            ->group(__DIR__.'/../routes/inertia.php');
-
-        Route::middleware(config('password-reset.api_middleware'))
-            ->prefix('vendor/password-reset')
-            ->group(__DIR__.'/../routes/api.php');
-
         Nova::serving(function () {
             $locale = app()->getLocale();
             $localeFile = lang_path('vendor/password-reset').'/'.$locale.'.json';
@@ -33,6 +28,20 @@ class ToolServiceProvider extends PackageServiceProvider
             File::exists($localeFile)
                 ? Nova::translations($localeFile)
                 : Nova::translations($filePath);
+
+            Nova::provideToScript(['minPasswordSize' => config('password-reset.min_password_size', 6)]);
         });
+    }
+
+    public function packageRegistered(): void
+    {
+        // Register router for view
+        Nova::router(config('password-reset.middleware'), 'password-reset')
+            ->group(__DIR__.'/../routes/inertia.php');
+
+        // Register router for api
+        Route::middleware(config('password-reset.api_middleware'))
+            ->prefix('vendor/password-reset')
+            ->group(__DIR__.'/../routes/api.php');
     }
 }
